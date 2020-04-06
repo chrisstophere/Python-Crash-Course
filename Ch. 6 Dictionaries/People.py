d5bdfa21dd034b4e3ee2f06d3fa6ff16ac805091
Chris Ewen <gcewen@gmail.com>
2020-04-06 12:06:29 -0400

    6-7. People

---
+++ Ch. 6 Dictionaries/People.py
@@ -0,0 +1,33 @@
+person1 = {
+            'first_name': 'Chris',
+            'last_name': 'Caserta',
+            'age': 47,
+            'city': 'Roxborough'
+          }
+
+person2 = {
+            'first_name': 'Tina',
+            'last_name': 'Ewen',
+            'age': 46,
+            'city': 'Jacksonville'
+          }
+
+person3 = {
+            'first_name': 'Jacob',
+            'last_name': 'Ewen',
+            'age': 13,
+            'city': 'Danville'
+          }
+
+people = [person1, person2, person3]
+
+#print(people)
+
+for items in people:
+    print(items)
+
+
+# print(person['first_name'])
+# print(person['last_name'])
+# print(person['age'])
+# print(person['city'])
