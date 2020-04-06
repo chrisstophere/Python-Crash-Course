7729822d47287b680aa7d0fa0b7f3b9b2903f6f9
Chris <chris@ewentech.com>
2020-04-06 13:32:46 -0400

    Update People.py

--- Ch. 6 Dictionaries/People.py
+++ Ch. 6 Dictionaries/People.py
@@ -1,33 +1,37 @@
+# Define an empty list
+people = []
+# Dictionary 1
 person1 = {
-            'first_name': 'Chris',
-            'last_name': 'Caserta',
+            'first_name': 'chris',
+            'last_name': 'caserta',
             'age': 47,
-            'city': 'Roxborough'
+            'city': 'roxborough'
           }
-
+# Add dictionary 1 to the people list
+people.append(person1)
+# Dictionary 2
 person2 = {
-            'first_name': 'Tina',
-            'last_name': 'Ewen',
+            'first_name': 'tina',
+            'last_name': 'ewen',
             'age': 46,
-            'city': 'Jacksonville'
+            'city': 'danville'
           }
-
+# Add dictionary 2 to the people list
+people.append(person2)
+# Dictionary 3
 person3 = {
-            'first_name': 'Jacob',
-            'last_name': 'Ewen',
+            'first_name': 'jacob',
+            'last_name': 'ewen',
             'age': 13,
-            'city': 'Danville'
+            'city': 'danville'
           }
-
-people = [person1, person2, person3]
-
-#print(people)
-
-for items in people:
-    print(items)
-
-
-# print(person['first_name'])
-# print(person['last_name'])
-# print(person['age'])
-# print(person['city'])
+# Add dictionary 3 to the people list
+people.append(person3)
+# Loop thru the people list
+for person in people:
+# Assign a variable to each value in the people list.
+    name = f"{person['first_name'].title()} {person['last_name'].title()}"
+    age = person['age']
+    city = person['city'].title()
+# Print a sentence using the variables created above.
+    print(f"{name}, of {city}, is {age} years old.")
